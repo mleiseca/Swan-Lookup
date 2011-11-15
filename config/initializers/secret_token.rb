b55b6ac12b2dc93fc54d722964ef244f7e1cca71
Michael Leiseca <mleiseca@mleiseca.gateway.2wire.net>
2011-11-15 01:41:19 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SwanlookupApp::Application.config.secret_token = 'c3ea528955f5a0028dfc194f60fa4a7a4d039f6986c021a1e1de865db23c3d664ec2800c041358c844cf314e2bd9721cfdc9dd8bf0cc86db3a68608ebdebbb57'
